2cf29619f1e7bdadae1465e6e1eb681db02ec3cb
Pawel <pmisiak@emarketer.com>
2021-09-24 15:04:50 -0400

    main initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 50,
+    singleQuote: true,
+    tabWidth: 1,
+    bracketSpacing: true,
+    semi: true,
+ //    printWidth: 150,
+ //    singleQuote: false,
+ //    tabWidth: 5,
+ //    bracketSpacing: false,
+ //    semi: false,
+ };
+ 
